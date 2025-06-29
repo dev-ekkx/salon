5d8736f0f6e7f95172729bf10ea4f8b2be98f418
Emmmanuel Kpendo <devekkx@gmail.com>
2025-06-29 16:31:12 +0000

    feature: add validation ui

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,69 +1,103 @@
-'use client'
+"use client";
 
-import React, {useState} from 'react'
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {FcGoogle} from "react-icons/fc";
-import {emailAndPhoneValidator} from "@/lib/utils";
-import parsePhoneNumber from "libphonenumber-js";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
+import { cn, emailAndPhoneValidator } from "@/lib/utils";
+import parsePhoneNumber, { CountryCode, E164Number } from "libphonenumber-js";
+import { useUserCountry } from "@/hooks/useCountries";
 
 const Page = () => {
-    // const {data: countries} = useCountries();
-    const [errorMessage, setErrorMessage] = useState("")
-    const [emailOrNumber, setEmailOrNumber] = useState<string | number>("")
-    const [isFormValid, setIsFormValid] = useState(false)
-    const [formValue, setFormValue] = useState()
+  // const {data: countries} = useCountries();
+  const { data: userCountryInfo } = useUserCountry();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [emailOrNumber, setEmailOrNumber] = useState<string | number>("");
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [formValue, setFormValue] = useState<E164Number | string>("");
 
-    const handleBlur = () => {
-        const validationCheck = emailAndPhoneValidator(emailOrNumber.toString().trim())
-        const data = validationCheck.data
-        if (validationCheck.key === "phone") {
-            console.log(parsePhoneNumber(data, 'GH'))
+  const handleBlur = () => {
+    const inputValue = emailOrNumber.toString().trim();
+    if (!inputValue.length) return;
 
-
-        } else {
-            console.log(data)
-            setErrorMessage(validationCheck.data)
-            setIsFormValid(false)
-        }
+    console.log(userCountryInfo);
+    const validationCheck = emailAndPhoneValidator(inputValue);
+    const data = validationCheck.data;
+    if (validationCheck.key === "phone") {
+      const phoneNumber = parsePhoneNumber(
+        data,
+        (userCountryInfo?.country ?? "US") as CountryCode
+      )?.number;
+      if (phoneNumber) {
+        setFormValue(phoneNumber);
+        setIsFormValid(true);
+        setErrorMessage("");
+      }
+    } else if (validationCheck.key === "email") {
+      setFormValue(data);
+      setIsFormValid(true);
+      setErrorMessage("");
+    } else {
+      console.log(data);
+      setErrorMessage(validationCheck.data);
+      setIsFormValid(false);
     }
+  };
 
-    return (
-        <div>
-            <section className="hidden md:flex md:justify-center md:items-center h-dvh">
-                <span className={"text-5xl"}>Switch to mobile to enjoy!!!</span>
-            </section>
-            <section
-                className={"bg-auth h-dvh bg-cover bg-center relative after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-black/40 md:hidden"}>
-                <section
-                    className={"p-4 sm:pl-10 sm:pr-32 transition-all duration-200 ease-initial flex absolute z-10 bottom-0 left-0 flex-col gap-4 h-6/12 w-full 2-20"}>
-                    <p className={"text-white font-bold text-3xl sm:text-4xl leading-10 h-auto"}>
-                        Book your perfect <br/> Look in minutes!
-                    </p>
-                    <div className="flex flex-col gap-4">
-                        <Input
-                            type={"text"}
-                            placeholder={"Enter your email or phone number"}
-                            value={emailOrNumber}
-                            onBlur={handleBlur}
-                            onChange={(e) => setEmailOrNumber(e.target.value)}
-                            className={"bg-white h-11"}/>
-                        <Button
-                            disabled={!isFormValid}
-                            className={"h-11 cursor-pointer"}>Continue</Button>
-                    </div>
-                    <span className={"font-bold text-2xl mx-auto text-white"}>OR</span>
-                    <Button
-                        className={"h-11 cursor-pointer overflow-x-clip flex gap-4 items-center font-semibold active:bg-primary"}
-                        variant={"outline"}>
-                        <FcGoogle/>
-                        <span>
-                        Continue with Google
-                        </span>
-                    </Button>
-                </section>
-            </section>
-        </div>
-    )
-}
-export default Page
+  return (
+    <div>
+      <section className="hidden h-dvh md:flex md:items-center md:justify-center">
+        <span className={"text-5xl"}>Switch to mobile to enjoy!!!</span>
+      </section>
+      <section
+        className={
+          "bg-auth relative h-dvh bg-cover bg-center after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-black/40 md:hidden"
+        }
+      >
+        <section
+          className={
+            "2-20 absolute bottom-0 left-0 z-10 flex h-6/12 w-full flex-col gap-4 p-4 transition-all duration-200 ease-initial sm:pr-32 sm:pl-10"
+          }
+        >
+          <p
+            className={
+              "h-auto text-3xl leading-10 font-bold text-white sm:text-4xl"
+            }
+          >
+            Book your perfect <br /> Look in minutes!
+          </p>
+          <div className="flex flex-col gap-4">
+            <Input
+              type={"text"}
+              placeholder={"Enter your email or phone number"}
+              value={emailOrNumber}
+              onBlur={handleBlur}
+              onChange={e => setEmailOrNumber(e.target.value)}
+              className={cn("h-11 bg-white", {
+                "border-red-500 text-red-500 outline-red-500":
+                  errorMessage.length > 0,
+              })}
+            />
+            {errorMessage.length > 0 && (
+              <p className={"-mt-4 text-sm text-red-500"}>{errorMessage}</p>
+            )}
+            <Button disabled={!isFormValid} className={"h-11 cursor-pointer"}>
+              Continue
+            </Button>
+          </div>
+          <span className={"mx-auto text-2xl font-bold text-white"}>OR</span>
+          <Button
+            className={
+              "active:bg-primary flex h-11 cursor-pointer items-center gap-4 overflow-x-clip font-semibold"
+            }
+            variant={"outline"}
+          >
+            <FcGoogle />
+            <span>Continue with Google</span>
+          </Button>
+        </section>
+      </section>
+    </div>
+  );
+};
+export default Page;
--- frontend/src/hooks/useCountries.tsx
+++ frontend/src/hooks/useCountries.tsx
@@ -1,10 +1,17 @@
-import {useQuery} from "@tanstack/react-query";
-import {Country} from "@/interfaces/country";
-import {fetchCountries} from "@/api/countries";
+import { useQuery } from "@tanstack/react-query";
+import { Country } from "@/interfaces/country";
+import { fetchCountries, fetchUserCountryInfo } from "@/api/countries";
 
 export const useCountries = () =>
-    useQuery<Country[]>({
-        queryKey: ['countries'],
-        queryFn: fetchCountries,
-        staleTime: 1000 * 60 * 10,
-    });
+  useQuery<Country[]>({
+    queryKey: ["countries"],
+    queryFn: fetchCountries,
+    staleTime: 1000 * 60 * 10,
+  });
+
+export const useUserCountry = () =>
+  useQuery({
+    queryKey: ["userCountry"],
+    queryFn: fetchUserCountryInfo,
+    staleTime: 1000 * 60 * 60, // 1 hour
+  });
